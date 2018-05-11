577212c986f9afe8ff475fe39396461ca1fdf623
OliCunnington <37498504+OliCunnington@users.noreply.github.com>
2018-05-12 00:46:40 +0100

    added / * + -
    abides bodmas (with no brackets)

--- app/src/main/java/com/example/eniac/calculator_layout/MainActivity.java
+++ app/src/main/java/com/example/eniac/calculator_layout/MainActivity.java
@@ -14,7 +14,13 @@ import android.widget.GridLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<Double> ints = new ArrayList<>();
+    private ArrayList<Character> operaters = new ArrayList<>();
+    private double result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +35,8 @@ public class MainActivity extends AppCompatActivity {
         if(txt.isEmpty()){
             txtV.setText(btn.getText());
         } else {
-            txtV.setText(txt+btn.getText());
+            String str = btn.getText().toString();
+            txtV.setText(txt+str);
         }
     }
 
@@ -41,7 +48,8 @@ public class MainActivity extends AppCompatActivity {
             case R.id.c_btn: case R.id.ce_btn: txtV.setText(""); break;
             case R.id.minus_btn: case R.id.plus_btn: case R.id.div_btn: case R.id.multi_btn: {
                 Button btn = (Button) v;
-                txtV.setText(txt + btn.getText());
+                String str = btn.getText().toString();
+                txtV.setText(txt + str);
                 break;
             }
             default:
@@ -51,14 +59,88 @@ public class MainActivity extends AppCompatActivity {
 
     public void onClickSpecial(View v){
         TextView txtV = findViewById(R.id.display);
-        Toast toast = Toast.makeText(this,txtV.getText().toString(), Toast.LENGTH_LONG);
+        String str = txtV.getText().toString();
+        txtV.setText("");
+        Toast toast = Toast.makeText(this, str, Toast.LENGTH_LONG);
         toast.setGravity(Gravity.TOP,0,5);
         toast.show();
-        try{
-            wait(1000);
-        } catch (Exception e){}
-        toast.setText("Calculating");
-        toast.show();
+        if (Character.isDigit(str.charAt(str.length()-1))) {
+            doCalculation(str);
+        }
+        else {
+            toast = Toast.makeText(this, "Please enter a Number at the end of the calculation", Toast.LENGTH_SHORT);
+            toast.show();
+        }
     }
 
+    public void doCalculation(String in){
+        ints.clear();
+        operaters.clear();
+        char[] chars = in.toCharArray();
+        StringBuilder strB = new StringBuilder();
+        int i=0;
+        for(;i<chars.length;i++){
+            if(Character.isDigit(chars[i])) strB.append((Character)chars[i]);
+            else if(chars[i]=='+'||chars[i]=='-'||chars[i]=='*'||chars[i]=='/'){
+                operaters.add(chars[i]);
+                ints.add(ints.size(), Double.parseDouble(strB.toString()));
+                strB.setLength(0);
+            }
+        }
+        ints.add(Double.parseDouble(strB.toString()));
+
+        while (!operaters.isEmpty()) {
+            if (operaters.contains('/')) {
+                int j = operaters.lastIndexOf('/');
+                divide(j);
+            } else if (operaters.contains('*')) {
+                int j = operaters.lastIndexOf('*');
+                multiply(j);
+            } else if (operaters.contains('-')) {
+                int j = operaters.lastIndexOf('-');
+                subtract(j);
+            }else if (operaters.contains('+')) {
+                int j = operaters.lastIndexOf('+');
+                addition(j);
+            }
+        }
+        result = ints.get(0);
+
+        Toast.makeText(this,result+"",Toast.LENGTH_SHORT).show();
+
+    }
+
+    private void divide(int i){
+        double d = ints.get(i)/ints.get(i+1);
+        ints.remove(i);
+        ints.remove(i);
+        operaters.remove(i);
+        ints.add(d);
+    }
+
+    private void multiply(int i){
+
+        double d = ints.get(i)*ints.get(i+1);
+        ints.remove(i);
+        ints.remove(i);
+        operaters.remove(i);
+        ints.add(d);
+    }
+
+    private void subtract(int i){
+
+        double d = ints.get(i)-ints.get(i+1);
+        ints.remove(i);
+        ints.remove(i);
+        operaters.remove(i);
+        ints.add(d);
+    }
+
+    private void addition(int i){
+        double d = ints.get(i)+ints.get(i+1);
+        ints.remove(i);
+        ints.remove(i);
+        operaters.remove(i);
+        ints.add(d);
+    }
 }
